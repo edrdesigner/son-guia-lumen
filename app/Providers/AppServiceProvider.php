4e2fe00bbe124ce271982fcf9e3863ab2aec75c9
VERITA\eduardo.reichert <eduardo.reichert@wssim.com.br>
2017-09-18 13:33:51 -0300

    Lumen - Criando observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,19 @@
 
 namespace App\Providers;
 
+namespace App\Restaurant;
+namespace App\Observers\RestaurantObserver;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
-{
+{	
+
+	public function boot()
+	{
+		Restaurant::observe(RestaurantObserver::class);
+	}
+
     /**
      * Register any application services.
      *
