62527bed777f2d9b0d731c63f474187c1db6dc04
Eduardo Reichert <edrdesigner@gmail.com>
2017-09-17 23:15:18 -0300

    First commit

---
+++ app/Http/Controllers/Api/V1/RestaurantsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\ApiControllerTrait;
+use App\Restaurant;
+use Illuminate\Http\Request;
+use Laravel\Lumen\Routing\Controller;
+
+
+class RestaurantsController extends Controller
+{
+    use ApiControllerTrait;
+    
+    protected $model;
+    protected $rules = [
+        'name' => 'required|min:3',
+        'description' => 'required'
+    ];
+
+    protected $messages = [
+        'required' => ':attribute obrigatorio',
+        'min' => ':attribute precisa de pelo menos :min caracteres'
+    ];
+
+    public function __construct(Restaurant $model)
+    {
+        $this->model = $model;
+    }
+}
